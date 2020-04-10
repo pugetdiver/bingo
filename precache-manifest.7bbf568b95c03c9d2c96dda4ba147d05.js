54096513d72844f1f5e05672198d49844135779c
Jason <jason_ingraham@hotmail.com>
2020-04-09 18:50:31 -0700

    Updates

---
+++ precache-manifest.7bbf568b95c03c9d2c96dda4ba147d05.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62d19d6189f2944735eb4a343d665bf9",
+    "url": "/bingo/index.html"
+  },
+  {
+    "revision": "85127540825e74bac00e",
+    "url": "/bingo/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "d9bdf2b8d2c68e93daf5",
+    "url": "/bingo/static/css/main.5faedd12.chunk.css"
+  },
+  {
+    "revision": "85127540825e74bac00e",
+    "url": "/bingo/static/js/2.06f4976f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bingo/static/js/2.06f4976f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9bdf2b8d2c68e93daf5",
+    "url": "/bingo/static/js/main.0bede6eb.chunk.js"
+  },
+  {
+    "revision": "c77dfdbe55a21accc6ad",
+    "url": "/bingo/static/js/runtime-main.c71c266f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bingo/static/media/logo.5d5d9eef.svg"
+  }
+]);
